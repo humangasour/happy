1e56c20e7592801b9d4e9e934e4a7ce7d66f5763
Anuj Sachdeva <anujsachdeva100@gmail.com>
2019-08-20 00:22:06 +0530

    initialized the landing page

---
+++ src/components/landing/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing.css';
+
+const landing = props => {
+    return (
+        <div className="landing-container">
+            <h1>Landing Page</h1>
+        </div>
+    )
+}
+
+export default landing;
